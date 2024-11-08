456c69cbb7dd374ab638ae8c2b41dd8ddc0bab2e
avi sit <avraham.sitton@post.runi.ac.il>
2024-11-08 09:12:43 +0200

    submission

--- FVCalc.java
+++ FVCalc.java
@@ -4,11 +4,11 @@ public class FVCalc {
 		int currentValue = Integer.parseInt(args[0]);
 		int rate = Integer.parseInt(args[1]);
         int years = Integer.parseInt(args[2]);
-
+        double converted = (double) rate; // המרה מפורשת
 		double futureValue = currentValue * Math.pow((1 + rate) , years);
 		int futureInt = (int) futureValue;
 		System.out.println("After " + years + " years, a $" + 
-		currentValue + " saved at " + rate + "% will yield $" + futureInt);
+		currentValue + " saved at " + rate + "%25 will yield $" + futureInt / 100);
 
 	}
 }
