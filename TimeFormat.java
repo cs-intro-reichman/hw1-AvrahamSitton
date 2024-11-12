e9e380fb21599d6f7399c28d1a84728095d6b64b
avi sit <avraham.sitton@post.runi.ac.il>
2024-11-12 21:31:07 +0200

    submission

--- TimeFormat.java
+++ TimeFormat.java
@@ -6,17 +6,17 @@ public class TimeFormat {
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
 			if (hours == 0 ){
               System.out.print("12:" + minutes + " AM");
-		}   else { if( hours < 10 ){
+		    }else if( hours < 10 ){
 				System.out.print(args[0].charAt(1) + ":" + minutes + " AM");
 			}else if(hours < 12 ){
-			  System.out.print(hours + minutes + " AM");
+			  System.out.print(hours + ":" + minutes + " AM");
+			}else { if (hours == 12){
+				System.out.print("12:" + minutes + " PM");
 			}else { if(hours < 24 ){
-				System.out.print(hours - 12 + minutes + " AM");
-
-			  }
+				System.out.print(hours - 12 + ":" + minutes + " PM");
+		
              
 			  }
-
 		}
 }
-}
+	}}
